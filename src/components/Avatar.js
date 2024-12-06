8153cd95c2f64be75b14a1e0de42cca010c10bbe
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-12-06 16:34:43 +0000

    Reverse changes in Avatar.js file

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,26 +1,21 @@
 import React from "react";
 import styles from "../styles/Avatar.module.css";
 
+
 const Avatar = ({ src, height = 50, text }) => {
-  return (
-    <span>
-      {src ? (
-        <img
-          className={styles.Avatar}
-          src={src}
-          height={height}
-          width={height}
-          alt="avatar"
-          onError={(e) => {
-            e.target.onerror = null; // Prevent infinite loop
-            e.target.style.display = "none"; // Hide image on error
-          }}
-        />
-      ) : (
-        <span className={styles.NoAvatar}>{text || "No Image Available"}</span>
-      )}
-    </span>
-  );
+ return (
+   <span>
+     <img
+       className={styles.Avatar}
+       src={src}
+       height={height}
+       width={height}
+       alt="avatar"
+     />
+     {text}
+   </span>
+ );
 };
+
 
 export default Avatar;
