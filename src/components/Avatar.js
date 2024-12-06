13bcbd3420276244f93bc333cc6bb46c88081ae4
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-12-06 16:17:44 +0000

    Remove image default option

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,21 +2,23 @@ import React from "react";
 import styles from "../styles/Avatar.module.css";
 
 const Avatar = ({ src, height = 50, text }) => {
-
   return (
     <span>
-      <img
-        className={styles.Avatar}
-        src={src || defaultImage}
-        height={height}
-        width={height}
-        alt="avatar"
-        onError={(e) => {
-          e.target.onerror = null;  // Prevent infinite loop
-          e.target.src = defaultImage;
-        }}
-      />
-      {text}
+      {src ? (
+        <img
+          className={styles.Avatar}
+          src={src}
+          height={height}
+          width={height}
+          alt="avatar"
+          onError={(e) => {
+            e.target.onerror = null; // Prevent infinite loop
+            e.target.style.display = "none"; // Hide image on error
+          }}
+        />
+      ) : (
+        <span className={styles.NoAvatar}>{text || "No Image Available"}</span>
+      )}
     </span>
   );
 };
