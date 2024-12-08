223e4d42911bd25ec1e41ca74bd0b64ed0e1fa2e
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-12-08 12:09:16 +0000

    Add hashtag display and content validation to post creation and page; add comments for clarity

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,3 +1,20 @@
+import React, { useRef, useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+import Asset from "../../components/Asset";
+import Upload from "../../assets/upload.png";
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import { useHistory } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useRedirect } from "../../hooks/useRedirect";
+
 function PostCreateForm() {
   useRedirect("loggedOut");  // Redirect users to login if not authenticated
   const [errors, setErrors] = useState({});
