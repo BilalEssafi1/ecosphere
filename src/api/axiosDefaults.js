500a6bb30ff34cbe4748de8500bd77b103730bf5
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-12-09 10:53:42 +0000

    Update axios to allow login from all browser

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,23 +1,44 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-green-social-61be33473742.herokuapp.com/";
+const baseURL = "https://drf-api-green-social-61be33473742.herokuapp.com/";
+
+// Default axios settings
+axios.defaults.baseURL = baseURL;
+axios.defaults.withCredentials = true;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.common["Accept"] = "application/json";
-axios.defaults.withCredentials = true;
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-// Create axios instances
+// Create axios instances with enhanced security
 export const axiosReq = axios.create({
   withCredentials: true,
   headers: {
-    Accept: "application/json",
+    "Accept": "application/json",
     "Content-Type": "application/json",
-  }
+    "X-Requested-With": "XMLHttpRequest"
+  },
+  credentials: 'include'
 });
 
 export const axiosRes = axios.create({
   withCredentials: true,
   headers: {
-    Accept: "application/json",
+    "Accept": "application/json",
     "Content-Type": "application/json",
-  }
+    "X-Requested-With": "XMLHttpRequest"
+  },
+  credentials: 'include'
+});
+
+// Add security headers to all requests
+[axiosReq, axiosRes].forEach(instance => {
+  instance.interceptors.request.use(config => {
+    config.headers = {
+      ...config.headers,
+      'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache'
+    };
+    return config;
+  });
 });
